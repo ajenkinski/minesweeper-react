27f9ee769e8bab7e8f53a9dc653b8e1d1a936643
ajenkinski <adam@thejenkins.org>
2020-07-14 23:20:43 -0400

    Removed source files

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App name="foobar" />);
-  const linkElement = getByText(/foobar/i);
-  expect(linkElement).toBeInTheDocument();
-});
