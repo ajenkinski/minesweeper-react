372828f58dd32e2d80d931ec9807bd04d00c94e1
ajenkinski <adam@thejenkins.org>
2020-08-05 22:08:34 -0400

    build

---
+++ precache-manifest.ccd46cbc8af8b39ab6e2606d9f463e65.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18f84bb7f3649e949f3672850c4a2b7",
+    "url": "/minesweeper-react/index.html"
+  },
+  {
+    "revision": "ed1b1dfad9df6fd87378",
+    "url": "/minesweeper-react/static/css/main.5b4fc93d.chunk.css"
+  },
+  {
+    "revision": "2146d1f3a72cfdcf46d3",
+    "url": "/minesweeper-react/static/js/2.73469454.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/minesweeper-react/static/js/2.73469454.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1b1dfad9df6fd87378",
+    "url": "/minesweeper-react/static/js/main.c154259a.chunk.js"
+  },
+  {
+    "revision": "50262dce385c77288bcd",
+    "url": "/minesweeper-react/static/js/runtime-main.7a63c1a2.js"
+  },
+  {
+    "revision": "b32b01e676c24df3d276647470f5c61f",
+    "url": "/minesweeper-react/static/media/flag.b32b01e6.svg"
+  },
+  {
+    "revision": "28570de765c9101ee5b6642eeb7d4788",
+    "url": "/minesweeper-react/static/media/mine.28570de7.svg"
+  }
+]);
